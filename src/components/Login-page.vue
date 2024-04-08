9b72c9e83d19de4500bdcecfcea92d97569d2632
Gean de Lima <117093951+Foccuns169@users.noreply.github.com>
2024-04-08 15:22:08 -0300

    :sparkles: feat: tela de login e rotas
    tela de login não finalizada

---
+++ src/components/Login-page.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-form 
+        class="loggin-container">
+        
+        <v-responsive
+        class="mx-auto"
+        max-width="344">
+        
+        <h1>Login</h1>
+
+        <v-text-field 
+          hint="Insira seu email de acesso"
+          label="Email" 
+          placeholder="johndoe@example.com" 
+          type="email" 
+          variant="outlined">
+       </v-text-field>
+
+       <v-text-field
+          hint="Insira sua senha para acessar"
+          label="Senha"
+          type="input"
+          variant="outlined">
+        </v-text-field>
+
+        <v-btn
+          class="btn-entrar"
+          color="green"
+          width="344">
+          Entrar
+        </v-btn>
+
+        <h3>Não possui cadastro?</h3>
+
+        <v-btn
+          class="btn-cadst"
+          color="orange"
+          width="172">
+          Cadastre-se
+        </v-btn>
+        </v-responsive>
+      </v-form>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -6,10 +6,31 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: () => import("../components/Login-page.vue"),
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: () => import("../components/Login-page.vue"),
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: () => import("../components/Registe-page.vue"),
+    },
+  ],
 })
+
+
+
 
 export default router
