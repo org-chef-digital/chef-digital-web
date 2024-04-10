0fa841d5efafa1f6fe168cae6fb293750a9e2ca4
Cleiton Silva <112819299+CleitonLucas@users.noreply.github.com>
2024-04-09 22:17:41 -0300

    :sparkles: feat: Layout da tela de cadastro

---
+++ src/components/Register-page.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container>
+    <v-navigation-drawer v-model="drawer" app permanent width="550" color="amber-darken-4">
+      <v-app-bar-title>Cadastre-se</v-app-bar-title>
+      <v-form class="form">
+        <v-text-field label="Nome Fantasia" variant="outlined" bg-color="white"></v-text-field>
+        <v-text-field label="Telefone (+DDD)" variant="outlined" bg-color="white"></v-text-field>
+        <v-text-field label="Email" variant="outlined" bg-color="white"></v-text-field>
+        <v-text-field label="Crie sua senha" type="password" variant="outlined" bg-color="white"></v-text-field>
+        <v-text-field label="Confirme sua senha" type="password" variant="outlined" bg-color="white"></v-text-field>
+        <div class="button-group">
+          <v-btn color="red">Cancelar</v-btn>
+          <v-btn color="green">Concluir</v-btn>
+        </div>
+      </v-form>
+    </v-navigation-drawer>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: true,
+    };
+  },
+};
+</script>
+
+<style>
+  .v-app-bar-title {
+    color: #000;
+    font-family: 'Montserrat Alternates';
+    text-align: center;
+    margin-top: 5%;
+  }
+
+  .v-text-field {
+    width: 60%;
+  }
+
+  .form {
+    margin-top: 8%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .button-group {
+    display: flex;
+    justify-content: space-between;
+    width: 60%;
+    margin-top: 8%;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -6,10 +6,17 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import('../components/Register-page.vue')
+    },
+  ]
 })
 
 export default router
