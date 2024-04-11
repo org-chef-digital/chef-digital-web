fe9a09f70c0ce301c89aeeb7f9dd13487adfe24c
Cleiton Silva <112819299+CleitonLucas@users.noreply.github.com>
2024-04-10 21:15:59 -0300

    :lipstick: style: finalização do layout da tela de cadastro

--- src/components/Register-page.vue
+++ src/components/Register-page.vue
@@ -1,55 +1,103 @@
 <template>
-  <v-container>
-    <v-navigation-drawer v-model="drawer" app permanent width="550" color="amber-darken-4">
-      <v-app-bar-title>Cadastre-se</v-app-bar-title>
-      <v-form class="form">
-        <v-text-field label="Nome Fantasia" variant="outlined" bg-color="white"></v-text-field>
-        <v-text-field label="Telefone (+DDD)" variant="outlined" bg-color="white"></v-text-field>
-        <v-text-field label="Email" variant="outlined" bg-color="white"></v-text-field>
-        <v-text-field label="Crie sua senha" type="password" variant="outlined" bg-color="white"></v-text-field>
-        <v-text-field label="Confirme sua senha" type="password" variant="outlined" bg-color="white"></v-text-field>
-        <div class="button-group">
-          <v-btn color="red">Cancelar</v-btn>
-          <v-btn color="green">Concluir</v-btn>
+  <v-responsive aspect-ratio="16/9">
+    <v-container class="page">
+      <v-navigation-drawer v-model="drawer" app permanent width="400" color="amber-darken-4">
+        <v-app-bar-title>Cadastre-se</v-app-bar-title>
+        <v-form class="form">
+          <v-text-field label="Nome Fantasia" variant="outlined" bg-color="white" required></v-text-field>
+
+          <v-text-field label="Telefone (+DDD)" variant="outlined" bg-color="white" required></v-text-field>
+
+          <v-text-field label="Email" variant="outlined" bg-color="white" required></v-text-field>
+
+          <v-text-field v-model="password" label="Crie sua senha" type="password" variant="outlined"
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[rules.required, rules.min8chars, rules.uppercase, rules.lowercase]"
+            :type="show1 ? 'text' : 'password'" bg-color="white" required></v-text-field>
+
+          <v-text-field v-model="confirmPassword" label="Confirme sua senha" type="password" variant="outlined"
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :rules="[rules.required, rules.matchPassword]"
+            :type="show1 ? 'text' : 'password'" bg-color="white" required></v-text-field>
+
+          <v-btn class="conclusion" color="green">Concluir</v-btn>
+        </v-form>
+        <div class="login">
+          <h4>Já tem uma conta?</h4>
+          <v-btn class="returnLogin" color="primary">Faça login</v-btn>
         </div>
-      </v-form>
-    </v-navigation-drawer>
-  </v-container>
+      </v-navigation-drawer>
+    </v-container>
+  </v-responsive>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      password: '',
+      confirmPassword: '',
       drawer: true,
+      show1: false,
+      rules: {
+        required: value => !!value || 'Campo obrigatório',
+        min8chars: value => value.length >= 8 || 'Mínimo de 8 caracteres',
+        uppercase: value => /[A-Z]/.test(value) || 'Pelo menos uma letra maiúscula',
+        lowercase: value => /[a-z]/.test(value) || 'Pelo menos uma letra minúscula',
+        matchPassword: (value) => value === this.password || 'As senhas não coincidem.',
+      },
     };
   },
+  methods: {
+
+  }
 };
 </script>
 
-<style>
-  .v-app-bar-title {
-    color: #000;
-    font-family: 'Montserrat Alternates';
-    text-align: center;
-    margin-top: 5%;
-  }
+<style scoped>
+.page {
+  background: url('../assets/backgron1.jpg');
+  background-size: cover;
+  height: 100vh;
+}
 
-  .v-text-field {
-    width: 60%;
-  }
+.login {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 8%;
+}
 
-  .form {
-    margin-top: 8%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
+h4 {
+  font-family: 'Roboto', sans-serif;
+  color: #000;
+  text-align: center;
+}
 
-  .button-group {
-    display: flex;
-    justify-content: space-between;
-    width: 60%;
-    margin-top: 8%;
-  }
+.v-app-bar-title {
+  color: #000;
+  font-size: 25px;
+  font-family: 'Roboto', sans-serif;
+  margin-top: 5%;
+  text-align: center;
+}
+
+.v-text-field {
+  margin: 0.2%;
+  width: 60%;
+}
+
+.form {
+  margin-top: 8%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.conclusion {
+  width: 60%;
+}
+
+.returnLogin {
+  margin-top: 2%;
+}
 </style>
