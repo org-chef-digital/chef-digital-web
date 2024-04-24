6533c235ff977c803a75768628963ae160888a55
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2024-04-23 21:40:48 -0300

    :book: feat:update axios base URL to use API version 1

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ import { registerPlugins } from '@/plugins'
 import App from './App.vue'
 
 import axios from 'axios'
-axios.defaults.baseURL = 'http://localhost:3000'
+axios.defaults.baseURL = 'http://localhost:3000/api/v1/'
 
 // Composables
 import { createApp } from 'vue'
