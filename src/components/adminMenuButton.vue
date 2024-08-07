166a925ab6e62fc1a2b881c4981ff32e76d691fa
Gean de Lima <117093951+Foccuns169@users.noreply.github.com>
2024-08-06 22:37:01 -0300

    feat: :sparkles: funcionalidade copiar link e logout
    Inserido o script responsável por realizar o logout da tela de administrador e a opção de copiar link do restaurante para compartilhamento

--- src/components/adminMenuButton.vue
+++ src/components/adminMenuButton.vue
@@ -22,7 +22,7 @@
                 </template>
                 <v-list>
                   <v-list-item>
-                    <v-switch hide-details></v-switch>
+                    <v-switch hide-details @click="toggleStatus"> Abrir/Fechar</v-switch>
                   </v-list-item>
                 </v-list>
               </v-menu>
@@ -40,13 +40,13 @@
               </v-btn>
             </v-list-item>
             <v-list-item>
-              <v-btn block class="menu-buttons">
+              <v-btn block class="menu-buttons" @click="logout">
                 <v-icon class="mr-4">mdi-logout</v-icon>
                 <span>Sair</span>
               </v-btn>
             </v-list-item>
             <v-list-item>
-              <v-btn block class="menu-buttons">
+              <v-btn block class="menu-buttons" @click="copyClientPageLink">
                 <v-icon class="mr-4" color="primary">mdi-link</v-icon>
                 <span class="link-button">Copiar link</span>
               </v-btn>
@@ -58,14 +58,35 @@
   </template>
   
   <script>
+  import router from '../router';
+  import { ref, onMounted } from 'vue';
   export default {
     name: 'adminMenuButton',
     methods: {
       createCategory() {
         this.$emit('create-category-1');
       },
+    
+      //copiar o link para o clinte
+      copyClientPageLink() {
+      const id = localStorage.getItem('id');
+      if (id) {
+        const clientPageLink = `${window.location.origin}/client/${id}`;
+        navigator.clipboard.writeText(clientPageLink);
+        console.log('Link copied:', clientPageLink);
+      } else {
+        console.error('Restaurant ID not found.');
+      }
+     },
+
+     //deslogar
+    logout() {
+      localStorage.removeItem('token');
+      localStorage.removeItem('id');
+      router.push({ name: 'login' });
     },
   }
+}
   </script>
   
   <style scoped>
